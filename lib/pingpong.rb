317d0d0b17288faa3040184e676d44ce8efd047a
Max Reichman and Peter Karellas <peter@>
2017-04-17 09:05:29 -0700

    Test all specs.

--- lib/pingpong.rb
+++ lib/pingpong.rb
@@ -3,9 +3,16 @@ class Fixnum
     ping_array = []
     numbers_entered = (1..self)
     numbers_entered.each() do |num|
-      if num.%(3).eql?(0)
+      if num.%(15).eql?(0)
+        ping_array.push("pingpong")
+      elsif num.%(3).eql?(0)
         ping_array.push("ping")
+      elsif num.%(5).eql?(0)
+        ping_array.push("pong")
+      else
+        ping_array.push(num)
       end
     end
+    ping_array
   end
 end
--- spec/pingpong_spec.rb
+++ spec/pingpong_spec.rb
@@ -3,6 +3,12 @@ require('pingpong')
 
 describe('Fixnum#pingpong') do
   it("When 3 is entered, ping is returned") do
-    expect((3).pingpong()).to(eq("ping"))
+    expect((3).pingpong()).to(eq([1, 2, "ping"]))
+  end
+  it("When 5 is entered, pong is returned") do
+    expect((5).pingpong()).to(eq([1, 2, "ping", 4, "pong"]))
+  end
+  it("When 15 is entered, pingpong is returned") do
+    expect((15).pingpong()).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"]))
   end
 end
